e0aab27dfefd57304d3c910119ecd9449b4ba9f5
Alex <mladtigyr@gmail.com>
2026-04-13 23:08:09 +0300

    Renaming DbContext and Seeding it with configurations

---
+++ EuroLeaguesScore.Data/EuroLeaguesScoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿namespace EuroLeaguesScore.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using Data.Models;
+
+    public class EuroLeaguesScoreDbContext : IdentityDbContext
+    {
+        public EuroLeaguesScoreDbContext(DbContextOptions<EuroLeaguesScoreDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Manager> Managers { get; set; } = null!;
+
+        public virtual DbSet<Team> Teams { get; set; } = null!;
+
+        public virtual DbSet<Player> Players { get; set; } = null!;
+
+        public virtual DbSet<League> Leagues { get; set; } = null!;
+
+        public virtual DbSet<Match> Matches { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+                base.OnModelCreating(builder);
+                
+                builder.ApplyConfigurationsFromAssembly(typeof(EuroLeaguesScoreDbContext).Assembly);
+        }
+
+    }
+}
