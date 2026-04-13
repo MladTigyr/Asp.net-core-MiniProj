d568f766d1a688a32e97d1b8d3631489d8744062
Alex <mladtigyr@gmail.com>
2026-04-13 21:58:47 +0300

    Adding Globally Common entityValidations

---
+++ EuroLeaguesScore.GCommon/EntityValidations.cs
@@ -0,0 +1,27 @@
+﻿namespace EuroLeaguesScore.GCommon
+{
+    public static class EntityValidations
+    {
+        //TeamValidations
+        public const int TeamNameMinLength = 3;
+        public const int TeamNameMaxLength = 50;
+    
+        public const int TeamCountryMinLength = 4;
+        public const int TeamCountryMaxLength = 56;
+    
+        public const int TeamCityMinLength = 2;
+        public const int TeamCityMaxLength = 150;
+
+        //LeagueValidations
+        public const int LeagueNameMinLength = 3;
+        public const int LeagueNameMaxLength = 50;
+
+        //PlayerValidations
+        public const int PlayerNameMinLength = 3;
+        public const int PlayerNameMaxLength = 100;
+
+        //ManagerValidations
+        public const int ManagerNameMinLength = 3;
+        public const int ManagerNameMaxLength = 100;
+    }
+}
