dce85a9274b44b2328a94aa51e4eb6467834ebd5
Alex <mladtigyr@gmail.com>
2026-04-16 02:18:38 +0300

    Added some messages for code reusability and code maintainability

---
+++ EuroLeaguesScore.GCommon/ViewModelsMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace EuroLeaguesScore.GCommon
+{
+    public static class ViewModelsMessages
+    {
+        public const string WinsLossesDrawsMessage = "{0} must be a positive number!";
+
+        public const string DeleteError = "An error occured while deleting this team";
+    }
+}
