29041494dda79e7a422df9a3c746398bc7f1dae4
Alex <mladtigyr@gmail.com>
2026-04-16 23:59:19 +0300

    Creating LeagueController with minimal bussiness logic

---
+++ EuroLeaguesScore/Controllers/LeagueController.cs
@@ -0,0 +1,43 @@
+﻿namespace EuroLeaguesScore.Controllers
+{
+    using EuroLeaguesScore.Services.Core.Contracts;
+    using EuroLeaguesScore.ViewModels.League;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class LeagueController : BaseController
+    {
+        private readonly ILeagueService leagueService;
+
+        public LeagueController(ILeagueService leagueService)
+        {
+            this.leagueService = leagueService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            IEnumerable<AllLeagueViewModel> models = await leagueService
+                .GetAllLeagueViewModelsOrderedByLeagueNameAsync();
+
+            return View(models);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            DetailsLeagueViewModel? model = await leagueService.GetDetailsLeagueViewModelAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+    }
+}
