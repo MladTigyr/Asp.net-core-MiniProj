d5e00e81475b81e3a60de78201654fb807b5d2bb
Alex <mladtigyr@gmail.com>
2026-04-16 23:56:49 +0300

    Creating ViewModels for the views of League

---
+++ EuroLeaguesScore.ViewModels/League/DetailsLeagueViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EuroLeaguesScore.ViewModels.League
+{
+    using EuroLeaguesScore.ViewModels.Team;
+
+    public class DetailsLeagueViewModel : AllLeagueViewModel
+    {
+        public int TotalPlayers { get; set; }
+
+        public IEnumerable<DetailsTeamInputModel> Teams { get; set; } 
+            = new HashSet<DetailsTeamInputModel>();
+    }
+}
