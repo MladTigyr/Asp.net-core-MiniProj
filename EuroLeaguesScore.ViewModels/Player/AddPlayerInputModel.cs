330725eda4063bb29888814c846514e71770b79b
Alex <mladtigyr@gmail.com>
2026-04-16 12:47:50 +0300

    Added view models for the views

---
+++ EuroLeaguesScore.ViewModels/Player/AddPlayerInputModel.cs
@@ -0,0 +1,38 @@
+﻿namespace EuroLeaguesScore.ViewModels.Player
+{
+    using System.ComponentModel.DataAnnotations;
+    using static GCommon.EntityValidations;
+    using static GCommon.ViewModelsMessages;
+    using Data.Models;
+    using EuroLeaguesScore.Data.Models.Enums;
+
+    public class AddPlayerInputModel
+    {
+        [Required]
+        [MinLength(PlayerNameMinLength)]
+        [MaxLength(PlayerNameMaxLength)]
+        public string Name { get; set; } = null!;
+        
+        [Required]
+        [Range(PlayerAgeMin, PlayerAgeMax, ErrorMessage = AgeMessage)]
+        public int Age { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required]
+        [Range(MinWinsLossesDraws, MaxWinsLossesDraws, ErrorMessage = GoalsAssistsMessage)]
+        public int Goals { get; set; }
+
+        [Required]
+        [Range(MinWinsLossesDraws, MaxWinsLossesDraws, ErrorMessage = GoalsAssistsMessage)]
+        public int Assists { get; set; }
+
+        [Required]
+        public int TeamId { get; set; }
+
+        [Required]
+        public IEnumerable<Team> TeamNames { get; set; }
+            = new HashSet<Team>();
+    }
+}
---
+++ EuroLeaguesScore.ViewModels/Player/AllPlayersViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace EuroLeaguesScore.ViewModels.Player
+{
+    public class AllPlayersViewModel : DetailsPlayerViewModel
+    {
+        public string LeagueName { get; set; } = null!;
+    }
+}
