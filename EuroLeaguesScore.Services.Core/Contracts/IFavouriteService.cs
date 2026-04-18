2a05f2c8752226f09f9fddaa5fd61f3429163bce
Alex <mladtigyr@gmail.com>
2026-04-18 03:29:21 +0300

    Creating FavouriteService and injecting it in program.cs (scoped). Adjusting some usings and styling for the newly views.

---
+++ EuroLeaguesScore.Services.Core/Contracts/IFavouriteService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace EuroLeaguesScore.Services.Core.Contracts
+{
+    using EuroLeaguesScore.Data.Models;
+    using EuroLeaguesScore.ViewModels.Favourite;
+
+    public interface IFavouriteService
+    {
+        public Task<IEnumerable<FavouriteTeamViewModel>?> GetAllFavTeamsIfExistAsync(string userId);
+
+        public Task ToggleTeamFavouriteAsync(string userId, int teamId);
+
+        public Task<IEnumerable<FavouritePlayerViewModel>?> GetAllFavPlayersIfExistAsync(string userId);
+
+        public Task TogglePlayerFavouriteAsync(string userId, int playerId);
+    }
+}
