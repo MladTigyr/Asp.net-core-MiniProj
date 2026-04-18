85078bbb4a5cb7d1dd25bd7c1c5d85a90232fdad
Alex <mladtigyr@gmail.com>
2026-04-18 03:31:24 +0300

    Creating FavouriteController with some User logic

--- EuroLeaguesScore/Controllers/BaseController.cs
+++ EuroLeaguesScore/Controllers/BaseController.cs
@@ -1,15 +1,30 @@
-﻿
-namespace EuroLeaguesScore.Controllers
+﻿namespace EuroLeaguesScore.Controllers
 {
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
 
     [Authorize]
-    public abstract class BaseController : Controller
+    public class BaseController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        protected bool IsAuthenticated()
+        {
+            return User.Identity?.IsAuthenticated ?? false;
+        }
+
+        protected string? GetUser()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+        protected string? GetUsername()
+        {
+            return User.Identity?.Name;
         }
     }
 }
---
+++ EuroLeaguesScore/Controllers/FavouriteController.cs
@@ -0,0 +1,99 @@
+﻿namespace EuroLeaguesScore.Controllers
+{
+    using EuroLeaguesScore.Data.Models;
+    using EuroLeaguesScore.Services.Core.Contracts;
+    using EuroLeaguesScore.ViewModels.Favourite;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class FavouriteController : BaseController
+    {
+        private readonly IFavouriteService favouriteService;
+
+        public FavouriteController(IFavouriteService favouriteService)
+        {
+            this.favouriteService = favouriteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Teams()
+        {
+            IEnumerable<FavouriteTeamViewModel>? teams = await favouriteService
+                .GetAllFavTeamsIfExistAsync(GetUser());
+
+            return View(teams);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ToggleTeam(int teamId)
+        {
+            if (teamId <= 0)
+            {
+                return BadRequest();
+            }
+
+            string userId = GetUser();
+
+            await favouriteService
+                .ToggleTeamFavouriteAsync(userId, teamId);
+
+            return RedirectToAction("All", "Team");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrRemoveTeam(int teamId)
+        {
+            if (teamId <= 0)
+            {
+                return BadRequest();
+            }
+
+            string userId = GetUser();
+
+            await favouriteService
+                .ToggleTeamFavouriteAsync(userId, teamId);
+
+            return RedirectToAction(nameof(Teams));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TogglePlayer(int playerId)
+        {
+            if (playerId <= 0)
+            {
+                return BadRequest();
+            }
+
+            string userId = GetUser();
+
+            await favouriteService
+                .TogglePlayerFavouriteAsync(userId, playerId);
+
+            return RedirectToAction("All", "Player");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Players()
+        {
+            IEnumerable<FavouritePlayerViewModel>? models = await favouriteService
+                .GetAllFavPlayersIfExistAsync(GetUser());
+
+            return View(models);
+        } 
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrRemovePlayer(int playerId)
+        {
+            if (playerId <= 0)
+            {
+                return BadRequest();
+            }
+
+            string userId = GetUser();
+
+            await favouriteService
+                .TogglePlayerFavouriteAsync(userId, playerId);
+
+            return RedirectToAction(nameof(Players));
+        }
+    }
+}
