52f78fd43cde8001adf5e91853ef702b0f017d2e
Alex <mladtigyr@gmail.com>
2026-04-18 03:25:12 +0300

    Creating ViewModels for the Views of Favourite feature

---
+++ EuroLeaguesScore.ViewModels/Favourite/FavouritePlayerViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EuroLeaguesScore.ViewModels.Favourite
+{
+    public class FavouritePlayerViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Age { get; set; }
+    }
+}
---
+++ EuroLeaguesScore.ViewModels/Favourite/FavouriteTeamViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EuroLeaguesScore.ViewModels.Favourite
+{
+    public class FavouriteTeamViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Country { get; set; } = null!;
+
+        public string LeagueName { get; set; } = null!;
+    }
+}
