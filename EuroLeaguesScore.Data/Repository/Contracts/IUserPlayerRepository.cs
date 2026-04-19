60926a57d0ef3f88ed479513b861bed77f89f50b
Alex <mladtigyr@gmail.com>
2026-04-19 17:38:44 +0300

    Created new repository for fav players

---
+++ EuroLeaguesScore.Data/Repository/Contracts/IUserPlayerRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace EuroLeaguesScore.Data.Repository.Contracts
+{
+    using EuroLeaguesScore.Data.Models;
+
+    public interface IUserPlayerRepository : IRepository<UserPlayer, string>
+    {
+        Task<IEnumerable<UserPlayer>> GetAllFavPlayersWithUserIdParamAsync(string userId);
+    }
+}
---
+++ EuroLeaguesScore.Data/Repository/UserPlayerRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace EuroLeaguesScore.Data.Repository
+{
+    using System.Linq;
+
+    using EuroLeaguesScore.Data.Models;
+    using EuroLeaguesScore.Data.Repository.Contracts;
+    using Microsoft.EntityFrameworkCore;
+
+    public class UserPlayerRepository : BaseRepository<UserPlayer, string>, IUserPlayerRepository
+    {
+        public UserPlayerRepository(EuroLeaguesScoreDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<UserPlayer>> GetAllFavPlayersWithUserIdParamAsync(string userId)
+        {
+            return await this.GetAllAttached()
+                .Where(x => x.UserId == userId)
+                .ToArrayAsync();
+        }
+    }
+}
