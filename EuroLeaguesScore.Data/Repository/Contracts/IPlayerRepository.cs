f24d3e03d7f9665d3e6b49354554ce4d95b362f7
Alex <mladtigyr@gmail.com>
2026-04-19 17:38:21 +0300

    Adding custom methods to the player repository

--- EuroLeaguesScore.Data/Repository/Contracts/IPlayerRepository.cs
+++ EuroLeaguesScore.Data/Repository/Contracts/IPlayerRepository.cs
@@ -5,5 +5,9 @@
     public interface IPlayerRepository : IRepository<Player, int>
     {
         Task<IEnumerable<Player>> GetDetailsPlayersOrderedByNameWithTeamIdAsync(int teamId);
+
+        Task<IEnumerable<Player>> GetAllPlayersOrderedByLeagueThenByTeamNameThenByNameAsync();
+
+        Task<Player?> GetPlayerWithHisTeamIfExistsAsync(int id);
     }
 }
