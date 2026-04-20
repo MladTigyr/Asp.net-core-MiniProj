f71d85ee7d05c3715fc39de5181b0c82049cc909
Alex <mladtigyr@gmail.com>
2026-04-20 20:56:39 +0300

    Added custom exception messages

--- EuroLeaguesScore.GCommon/ExceptionMessages.cs
+++ EuroLeaguesScore.GCommon/ExceptionMessages.cs
@@ -9,5 +9,7 @@ namespace EuroLeaguesScore.GCommon
     public static class ExceptionMessages
     {
         public const string RoleSeedingExceptionMessage = "An error occured while trying to seed the role {0}!";
+        public const string AdminAssignExceptionMessage = "Failed to assign admin role to user: {0}!";
+        public const string AdminCreationExceptionMessage = "Failed to create admin user: {0}";
     }
 }
