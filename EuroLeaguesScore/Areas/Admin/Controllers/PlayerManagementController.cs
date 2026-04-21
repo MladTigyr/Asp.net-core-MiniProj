6bcfd1891dcf58e7803e75633012bece0761064a
Alex <mladtigyr@gmail.com>
2026-04-21 06:19:26 +0300

    Implementing PlayerManagement menu

---
+++ EuroLeaguesScore/Areas/Admin/Controllers/PlayerManagementController.cs
@@ -0,0 +1,29 @@
+﻿
+namespace EuroLeaguesScore.Areas.Admin.Controllers
+{
+    using EuroLeaguesScore.Services.Core.Contracts;
+
+    using EuroLeaguesScore.ViewModels.Player;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PlayerManagementController : BaseController
+    {
+        private readonly IPlayerService playerService;
+
+        public PlayerManagementController(IPlayerService playerService)
+        {
+            this.playerService = playerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            string adminId = GetAdminUserId();
+
+            IEnumerable<AllPlayersViewModel> models = await playerService
+                .GetAllPlayersOrderedByLeagueThenByTeamNameThenByNameAsync(adminId);
+
+            return View(models);
+        }
+    }
+}
