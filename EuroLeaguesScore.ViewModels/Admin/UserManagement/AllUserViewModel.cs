62d56c20a0306001c808a095e0a8baffa1198db9
Alex <mladtigyr@gmail.com>
2026-04-21 06:15:50 +0300

    Creating userViewModel for the UserManagement implementation

---
+++ EuroLeaguesScore.ViewModels/Admin/UserManagement/AllUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EuroLeaguesScore.ViewModels.Admin.UserManagement
+{
+    public class AllUserViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Role { get; set; } = null!;
+    }
+}
