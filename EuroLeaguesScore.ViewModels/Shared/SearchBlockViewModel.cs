229ad2d982178a59da047ff6478e514b4601e48f
Alex <mladtigyr@gmail.com>
2026-04-21 08:25:12 +0300

    Creating partial filtering views

---
+++ EuroLeaguesScore.ViewModels/Shared/SearchBlockViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EuroLeaguesScore.ViewModels.Shared
+{
+    public class SearchBlockViewModel
+    {
+        public string? SearchTerm { get; set; }
+
+        public string Controller { get; set; } = null!;
+
+        public string Action { get; set; } = null!;
+
+        public string Placeholder { get; set; } = "Search...";
+    }
+}
