c98f28bca4d7600dc97d29e3267afe2c732f1e48
Alex <mladtigyr@gmail.com>
2026-04-21 06:11:59 +0300

    Creating Admin Dashboard page and updating Header in _layout

---
+++ EuroLeaguesScore/Areas/Admin/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EuroLeaguesScore.Areas.Admin
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
